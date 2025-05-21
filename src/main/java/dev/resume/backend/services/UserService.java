36903fa012fd1d332dbad0a9bab460b708ee5508
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-05-21 00:20:01 -0400

    feat(user): update create method and add getById in UserService

--- src/main/java/dev/resume/backend/services/UserService.java
+++ src/main/java/dev/resume/backend/services/UserService.java
@@ -1,24 +1,42 @@
 package dev.resume.backend.services;
 
+import dev.resume.backend.dto.UserRequestDTO;
+import dev.resume.backend.dto.UserResponseDTO;
 import dev.resume.backend.entities.UserEntity;
 import dev.resume.backend.exceptions.UserAlreadyExistException;
+import dev.resume.backend.mappers.UserMapper;
 import dev.resume.backend.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserEntity create(UserEntity userEntity){
-        var user = this.userRepository.findByEmail((userEntity.getEmail()));
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
 
-        if(user != null){
+    public UserResponseDTO getUserById(UUID id) {
+        UserEntity user = userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("User with id " + id + " not found"));
+
+        return userMapper.toResponse(user);
+    }
+
+    public UserResponseDTO create(UserRequestDTO userDTO) {
+        if (userRepository.findByEmail(userDTO.email()) != null) {
             throw new UserAlreadyExistException();
         }
 
-        return userRepository.save(userEntity);
+        UserEntity user = userMapper.toEntity(userDTO);
+        UserEntity savedUser = userRepository.save(user);
+        return userMapper.toResponse(savedUser);
     }
+
+
 }

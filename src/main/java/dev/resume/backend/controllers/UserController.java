8a5a04073e87e12739b12a91aa9182de4f546fac
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-05-21 00:37:15 -0400

    feat(user): update create endpoint to use UserRequestDTO and return UserResponseDTO

--- src/main/java/dev/resume/backend/controllers/UserController.java
+++ src/main/java/dev/resume/backend/controllers/UserController.java
@@ -1,11 +1,16 @@
 package dev.resume.backend.controllers;
 
-import dev.resume.backend.entities.UserEntity;
+import dev.resume.backend.dto.UserRequestDTO;
+import dev.resume.backend.dto.UserResponseDTO;
 import dev.resume.backend.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
@@ -15,12 +20,8 @@ public class UserController {
     private UserService userService;
 
     @PostMapping()
-    public ResponseEntity create(@Valid @RequestBody UserEntity userEntity){
-        try {
-            var result = userService.create(userEntity);
-            return ResponseEntity.created(null).body(result);
-        } catch(Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<UserResponseDTO> create(@Valid @RequestBody UserRequestDTO userDTO) {
+        UserResponseDTO response = userService.create(userDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }

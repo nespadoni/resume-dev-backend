2449c26de576e6676e04cb68b59f03c1ec6346da
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-05-21 03:43:31 -0400

    feat(controller): enhance UserController with email verification and CRUD operations

--- src/main/java/dev/resume/backend/controllers/UserController.java
+++ src/main/java/dev/resume/backend/controllers/UserController.java
@@ -1,27 +1,55 @@
 package dev.resume.backend.controllers;
 
+import dev.resume.backend.dto.CreateUserResponseDTO;
 import dev.resume.backend.dto.UserRequestDTO;
 import dev.resume.backend.dto.UserResponseDTO;
+import dev.resume.backend.dto.VerifyEmailRequestDTO;
 import dev.resume.backend.services.UserService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping()
-    public ResponseEntity<UserResponseDTO> create(@Valid @RequestBody UserRequestDTO userDTO) {
-        UserResponseDTO response = userService.create(userDTO);
+    public ResponseEntity<CreateUserResponseDTO> create(@Valid @RequestBody UserRequestDTO userDTO) {
+        CreateUserResponseDTO response = userService.create(userDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @PostMapping("/verify-email")
+    public ResponseEntity<String> verifyEmail(@RequestBody VerifyEmailRequestDTO request) {
+        String response = userService.verifyEmail(request.email(), request.code());
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable String id) {
+        UserResponseDTO response = userService.getUserById(UUID.fromString(id));
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> update(@PathVariable String id, @Valid @RequestBody UserRequestDTO userDTO) {
+        UserResponseDTO response = userService.update(UUID.fromString(id), userDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        userService.delete(UUID.fromString(id));
+        return ResponseEntity.noContent().build();
+    }
+
 }
